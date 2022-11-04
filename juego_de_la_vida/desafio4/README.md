027430cd45a081eb96ebd5a1c46d2d3b268c690b
Lucio J. Quintana <quintana.unq.92@gmail.com>
2022-11-04 19:07:54 -0300

    agregado el readme con la descripcion. Editado el archivo para ejecutar python correctamente.

---
+++ juego_de_la_vida/desafio4/README.md
@@ -0,0 +1,17 @@
+# Game of Life/Juego de la vida
+
+
+EL juego de la vida consiste en un simulador del proceso de crear una proteína a partir de una cadena ADN, de manera extremadamente simple
+en el que solo tendremos en cuenta la union entre nucleotidos.
+Cada codón formado correctamente sumará puntos.
+
+Una fase sera la de formar la cadena de ARNm y, en el nivel bonus, la cadena de ARNt que culminara con la Proteina"
+Es necesario tener instalado Python para ejecutar el programa
+
+Si no estas familiarizado en el tema, el juego ofrece un tutorial para poder comenzar.
+
+## Pasos
+
+1. Descargá este repositorio, usando la terminal.
+2. Andá al directorio del proyecto y ejecutá `gameOfLife.sh`
+3. Seguí las instrucciones. ¡Diviertete!
--- juego_de_la_vida/desafio4/gameOfLife.sh
+++ juego_de_la_vida/desafio4/gameOfLife.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-function ayuda(){
+function help(){
 	echo -e "EL juego de la vida consiste en un simulador del proceso de crear una proteína a partir de una cadena ADN."
 	echo -e "Cada codón formado correctamente sumará puntos."
-	echo -e "Una fase sera la de formar la cadena de ARNm y, en el nivel bonus, la cadena de Proteina."
+	echo -e "Una fase sera la de formar la cadena de ARNm y, en el nivel bonus, la cadena de ARNt."
 	echo -e "Es necesario tener instalado Python para ejecutar el programa"
 	echo -e "Escribe en la consola ./juegoDeLaVida.sh para poder jugar!"
 	echo -e "diviertete!"
@@ -10,7 +10,7 @@ function ayuda(){
 
 if [ "$1" != "--help" ] && [ "$1" != "--h" ]
 	then
-		python3 juegoDeLaVida.py
+		python3.10 gameOfLife.py
 	else
-		ayuda
+		help
 fi
